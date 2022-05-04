a12f31b91fa70b64b8c7b73819d5454dc5627c61
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-05-04 11:03:37 -0300

    F

--- microservices/notification/email/src/mail/mail.service.ts
+++ microservices/notification/email/src/mail/mail.service.ts
@@ -1,6 +1,6 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { User } from './../user/user.entity';
+
 
 @Injectable()
 export class MailService {
@@ -10,12 +10,12 @@ export class MailService {
     const url = `example.com/auth/confirm?token=${token}`;
 
     await this.mailerService.sendMail({
-      to: user.email,
+      to: "danielbatistagalvao@hotmail.com",
       // from: '"Support Team" <support@example.com>', // override default from
       subject: 'Welcome to Nice App! Confirm your Email',
       template: './confirmation', // `.hbs` extension is appended automatically
       context: { // ✏️ filling curly brackets with content
-        name: user.name,
+        name: "Daniel",
         url,
       },
     });
